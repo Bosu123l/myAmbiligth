d92992766f3cdd434e976b3232e329663fec32f1
Łukasz Gawin <gawinster@gmail.com>
2015-10-11 00:54:49 +0200

    algorytm czytania kolorow
    +dodany widok funkcionujacy z koloroami na 24 diody
    +algorytm obliczajacy kolory w przestrzeniach kwadratów
    +bidowanie widoku z lista stiringów

--- ProjektInzynierski/ColorCalculate.cs
+++ ProjektInzynierski/ColorCalculate.cs
@@ -3,36 +3,190 @@ using System.Collections.ObjectModel;
 using SlimDX;
 using System.Drawing;
 using SlimDX.Direct3D9;
+using System.Collections.Generic;
 
 namespace ProjektInzynierski
 {
     public class ColorCalculate
     {
         private ScreenCapture _screenCapture;
-        private readonly Surface _surface;
-        private DataRectangle _dataRectangle;
-        private DataStream _dataStream;
+
+
+
+        private const int Bpp = 4;
+        private const int NumberOfLedInLine = 7;
+        private const int NumberOfLeds = 24;
+
+        private List<Collection<long>> ListOfPositions = new List<Collection<long>>();
 
         public ColorCalculate(ScreenCapture screenCapture)
         {
-            if(screenCapture==null)
+            if (screenCapture == null)
                 throw new ArgumentNullException("screenCapture null");
             _screenCapture = screenCapture;
-            _surface = _screenCapture.CaptureScreen();
-            _dataRectangle = _surface.LockRectangle(LockFlags.None);
-            _dataStream = _dataRectangle.Data;
+          
+
+
+
+            for (int i = 0; i < NumberOfLeds; i++)
+            {
+                ListOfPositions.Add(new Collection<long>());
+            }
+
+            int margin = 8;
+
+            int _widthScreenSize = Convert.ToInt32(System.Windows.SystemParameters.PrimaryScreenWidth) - margin;
+            int _heightScreenSize = Convert.ToInt32(System.Windows.SystemParameters.PrimaryScreenHeight) - margin;
+
+            int[] _collumnPosition = new int[7];
+            int[] _rowPosition = new int[7];
+
+            for (int i = 0; i < NumberOfLedInLine; i++)
+            {
+                _collumnPosition[i] = ((_widthScreenSize - margin) * i) / NumberOfLedInLine + margin;
+                _rowPosition[i] = ((_heightScreenSize - margin) * i) / NumberOfLedInLine + margin;
+            }
+
+            long x, y;
+            long step = 5;
+            long position;
+
+            y = margin;
+            for (x = margin; x < _widthScreenSize; x += step)
+            {
+                position = (y * Convert.ToInt32(System.Windows.SystemParameters.PrimaryScreenWidth) + x) * Bpp;
+
+                int z;
+
+                for (z = 0; z < NumberOfLedInLine - 1; z++)
+                {
+                    if (x >= _collumnPosition[z] && x < _collumnPosition[z + 1])
+                    {
+
+                        ListOfPositions[z].Add(position);
+                        z = NumberOfLedInLine;
+
+                    }
+
+                }
+                if (z == NumberOfLedInLine - 1)
+                {
+                    ListOfPositions[z].Add(position);
+                }
+            }
+
+            y = _heightScreenSize;
+
+            for (x = margin; x < _widthScreenSize; x += step)
+            {
+                position = (y * Convert.ToInt32(System.Windows.SystemParameters.PrimaryScreenWidth) + x) * Bpp;
+
+                int z;
+
+                for (z = 0; z < NumberOfLedInLine - 1; z++)
+                {
+                    if (x >= _collumnPosition[z] && x < _collumnPosition[z + 1])
+                    {
+
+                        ListOfPositions[18 - z].Add(position);
+                        z = NumberOfLedInLine;
+
+                    }
+
+                }
+                if (z == NumberOfLedInLine - 1)
+                {
+                    ListOfPositions[18 - z].Add(position);
+                }
+            }
+
+
+
+            x = margin;
+            for (y = margin + 1; y < _heightScreenSize; y += step)
+            {
+                position = (y * Convert.ToInt32(System.Windows.SystemParameters.PrimaryScreenWidth) + x) * Bpp;
+
+                int z;
+
+                for (z = 0; z < NumberOfLedInLine - 1; z++)
+                {
+                    if (y >= _rowPosition[z] && y < _rowPosition[z + 1])
+                    {
+                        if (z == 0)
+                        {
+                            ListOfPositions[0].Add(position);
+                        }
+                        else
+                        {
+                            ListOfPositions[24 - z].Add(position);
+
+                        }
+                        z = NumberOfLedInLine;
+
+
+                    }
+
+                }
+                if (z == NumberOfLedInLine - 1)
+                {
+                    ListOfPositions[24 - z].Add(position);
+                }
+            }
+
+
+
+            x = _widthScreenSize;
+
+            for (y = margin + 1; y < _heightScreenSize; y += step)
+            {
+                position = (y * Convert.ToInt32(System.Windows.SystemParameters.PrimaryScreenWidth) + x) * Bpp;
+
+                int z;
+
+                for (z = 0; z < NumberOfLedInLine - 1; z++)
+                {
+                    if (y >= _rowPosition[z] && y < _rowPosition[z + 1])
+                    {
+
+                        ListOfPositions[6 + z].Add(position);
+
+                        z = NumberOfLedInLine;
+
+
+                    }
+
+                }
+                if (z == NumberOfLedInLine - 1)
+                {
+                    ListOfPositions[6 + z].Add(position);
+                }
+            }
 
         }
 
-        public void Calculate()
+        public List<string> Calculate()
         {
-            //TODO : dodac liste colekcji  w argumencie  i zwrocic ja po przeliczeniu wartosci 
 
+            Surface _surface = _screenCapture.CaptureScreen();
+            DataRectangle _dataRectangle = _surface.LockRectangle(LockFlags.None);
+            DataStream dataStream = _dataRectangle.Data;
+
+            List<string> listOfColors = new List<string>();
+
+            foreach (Collection<long> collection in ListOfPositions)
+            {
+                var buff = AvgColor(dataStream,collection).ToString();
+                //          string color
+                listOfColors.Add(buff);
+            }
             _surface.UnlockRectangle();
             _surface.Dispose();
+            return listOfColors;
         }
-        private Color AvgColor(DataStream dataStream, Collection<long> positions)
+        private System.Windows.Media.Color AvgColor(DataStream dataStream, Collection<long> positions)
         {
+      
             byte[] bu = new byte[4];
             int r = 0;
             int g = 0;
@@ -47,9 +201,12 @@ namespace ProjektInzynierski
                 g += bu[1];
                 b += bu[0];
                 i++;
+                dataStream.Position = 0;
             }
-
-            return Color.FromArgb(r / i, g / i, b / i);
+            byte red = (byte)(r / i);
+            byte green = (byte)(g / i);
+            byte blue = (byte)(b / i);
+            return System.Windows.Media.Color.FromArgb(255, red, green, blue);
         }
     }
 }
--- ProjektInzynierski/View/UserView.xaml.cs
+++ ProjektInzynierski/View/UserView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,33 +21,78 @@ namespace ProjektInzynierski.View
     /// <summary>
     /// Interaction logic for UserView.xaml
     /// </summary>
-    public partial class UserView : UserControl
+    public partial class UserView : UserControl, INotifyPropertyChanged
     {
         private ScreenCapture _screenCapture;
         private ColorCalculate _colorCalculate;
+        private List<string> _colorOfScreen;
+        public List<string> ColorOfScreen
+        {
+            get
+            {
+                return _colorOfScreen;
+            }
+            set
+            {
+                if (_colorOfScreen != value)
+                {
+                    _colorOfScreen = value;
+                    NotifyPropertyChanged("ColorOfScreen");
+                }
+            }
+        }
         private Timer _timer;
-        public string Cos
+        public string resolution
         {
             get;
             set;
         }
+        private Thickness _marginTop;
+        public Thickness MarginTop
+        {
+            get
+            {
+                return _marginTop;
+            }
+            set
+            {
+                if (_marginTop != value)
+                {
+                    _marginTop = value;
+                    NotifyPropertyChanged("MarginTop");
+                }
+            }
+        }
 
-    
+
 
         public UserView()
         {
-            _timer=new Timer(100);//1000 =1s
+            ColorOfScreen = new List<string>();
+            _timer = new Timer(100);//1000 =1s
             _timer.Elapsed += _timer_Elapsed;
-            Cos = "Janusz";
             _screenCapture = new ScreenCapture();
-            _colorCalculate=new ColorCalculate(_screenCapture);
+            _colorCalculate = new ColorCalculate(_screenCapture);
+
+            resolution = System.Windows.SystemParameters.PrimaryScreenWidth.ToString() + " x " + System.Windows.SystemParameters.PrimaryScreenHeight.ToString();
+
+
             InitializeComponent();
             _timer.Start();
         }
 
         private void _timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-           //TODO:  obliczenie z klasy ColorCalculate
+            ColorOfScreen = _colorCalculate.Calculate();
         }
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            var tempHandler = PropertyChanged;
+            if (tempHandler != null)
+            {
+                tempHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
